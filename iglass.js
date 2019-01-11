994bdcf9456e582d747c19f110c6c3d515891338
Shea Ramage <shea.ramage@gmail.com>
2019-01-11 17:05:16 -0500

    Bugfix, starting on new js feature

--- iglass.js
+++ iglass.js
@@ -1,19 +1,29 @@
-var tx = document.getElementsByClassName("clickForPopup")[0].getElementsByTagName("span")[0].innerText;
-var usnr = document.getElementsByClassName("clickForPopup")[1].getElementsByTagName("span")[0].innerText;
-
-//DOWNSTREAM
-var rx = document.getElementsByClassName("clickForPopup")[2].getElementsByTagName("span")[0].innerText;
-var dsnr = document.getElementsByClassName("clickForPopup")[3].getElementsByTagName("span")[0].innerText;
-
-// As a string
-var upstr = "Upstream Power: " + tx + ", SNR: " + usnr;
-var downstr = "Downstream Power: " + rx + ", SNR: " + dsnr;
-var signalstr = upstr + " || " + downstr;
-
-var signalButton = "<button id='signalButton' class='ui-button ui-corner-all ui-widget' onclick='copySignal()'>Copy Signals</button>";
-document.getElementById('expandAll').insertAdjacentHTML('beforebegin', signalButton);
-
 var contentDiv = document.getElementById("content");
+var headerText = document.getElementsByClassName("ui-widget-header")[0].innerText;
+
+if (document.getElementsByClassName("clickForPopup").length > 0) {
+    var tx = document.getElementsByClassName("clickForPopup")[0].getElementsByTagName("span")[0].innerText;
+    var usnr = document.getElementsByClassName("clickForPopup")[1].getElementsByTagName("span")[0].innerText;
+    
+    //DOWNSTREAM
+    var rx = document.getElementsByClassName("clickForPopup")[2].getElementsByTagName("span")[0].innerText;
+    var dsnr = document.getElementsByClassName("clickForPopup")[3].getElementsByTagName("span")[0].innerText;
+    
+    // As a string
+    var upstr = "Upstream Power: " + tx + ", SNR: " + usnr;
+    var downstr = "Downstream Power: " + rx + ", SNR: " + dsnr;
+    var signalstr = upstr + " || " + downstr;
+    
+    var signalButton = "<button id='signalButton' class='ui-button ui-corner-all ui-widget' onclick='copySignal()'>Copy Signals</button>";
+    document.getElementById('expandAll').insertAdjacentHTML('beforebegin', signalButton);
+}
+else if (headerText.substring(0,12) === "Modem Events"){
+    document.getElementsByTagName("span").style.setAttribute("style", "background: #FFFFFF;")
+    console.log("Beep");
+}
+else {
+    console.log("Not a modem page");
+}
 
 function copySignal() {
     var signalArea = document.createElement('textarea');
