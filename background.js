550846a360f6eda3a4f851f6cf00843d17be567c
Shea Ramage <shea.ramage@gmail.com>
2019-01-18 11:30:49 -0500

    More work on POTD

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
-chrome.commands.onCommand.addListener(function(command) {
-    console.log('Command:', command);
-  });
+chrome.runtime.onInstalled.addListener(function() {
+    let txt = '';
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+        if (xmlhttp.status == 200 && xmlhttp.readyState == 4){
+                txt = xmlhttp.responseText;
+                alert(txt);
+            }
+    };
+    xmlhttp.open("GET", "http://127.0.0.1:5000/potd", true);
+    xmlhttp.send();
+    alert(xmlhttp.responseText);
+});
