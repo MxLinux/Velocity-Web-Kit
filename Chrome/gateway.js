2453b2bd86aa632f89cb14cf38a06fd581a28fa9
Shea Ramage <shea.ramage@gmail.com>
2019-01-26 17:23:15 -0500

    Fixed up gateway login

--- Chrome/gateway.js
+++ Chrome/gateway.js
@@ -5,27 +5,23 @@ var match = site.match(regex);
 
 if (match != null) {
     if (location.port == 8080) {
-        const xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = () => {
-            if (xhttp.readyState === 4) {
-                xhttp.status === 200 ? console.log(xhttp.responseText) : console.error('Error')
-                setTimeout(function() {
-                    var login_node = document.getElementById("Login");
-                    if (typeof(login_node) != 'undefined' && login_node != null) {
+        setTimeout(function() {
+            var login_node = document.getElementById("Login");
+            if (typeof(login_node) != 'undefined' && login_node != null) {
+                const xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = () => {
+                    if (xhttp.readyState === 4) {
+                        xhttp.status === 200 ? console.log(xhttp.responseText) : console.error('Error')
                         document.getElementById('Password').value = "" + xhttp.responseText;
                         document.getElementById('UserName').value = "technician";
-                        document.querySelector('.submitBtn').click();                        
+                        if (document.getElementById('Password').value != null && String(document.getElementById('Password').value).length == 10) {
+                            document.querySelector('.submitBtn').click();                        
+                        }
                     }
-                    else {
-                        console.log("Not a gateway login...");
-                    }
-                }, 1000);
-            }
-        }
-        xhttp.open("GET", "http://127.0.0.1/potd", true);
-        xhttp.send();
+                }
+                xhttp.open("GET", "http://127.0.0.1/potd", true);
+                xhttp.send();
+            }                    
+        }, 1000);
     }
-}
-else {
-    console.log("Not a gateway login.")
 }
