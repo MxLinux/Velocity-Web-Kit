fca68de6f583cd51657901b667fdb4a1bb81110d
Shea Ramage <shea.ramage@gmail.com>
2019-01-29 14:51:47 -0500

    Start working on outage data for iGlass

--- Chrome/gateway.js
+++ Chrome/gateway.js
@@ -1,24 +1,34 @@
 var user = "technician";
 var site = location.hostname;
+// Some regex I totally stole from SO to check against Class A addresses
 var regex = new RegExp("(10)(\.([2]([0-5][0-5]|[01234][6-9])|[1][0-9][0-9]|[1-9][0-9]|[0-9])){3}");
 var match = site.match(regex);
 
 if (match != null) {
-    if (location.port == 8080) {
+    // Check if this is the web interface port, port 80 should be default with FEC counter
+    if (location.port == 8080) { 
+        // Wait 1 second before trying, elements on page take a while to load
         setTimeout(function() {
-            var login_node = document.getElementById("Login");
-            if (typeof(login_node) != 'undefined' && login_node != null) {
+            var loginNode = document.getElementById("Login");
+            // Make sure login div exists on this page/has loaded
+            if (typeof(loginNode) != 'undefined' && loginNode != null) {
+                // Send XHR to the POTD-Web flask server
                 const xhttp = new XMLHttpRequest();
                 xhttp.onreadystatechange = () => {
+                    // Checks XHR status to determine that we have successfully loaded the POTD page
                     if (xhttp.readyState === 4) {
                         xhttp.status === 200 ? console.log(xhttp.responseText) : console.error('Error')
+                        // Fill out credentials 
                         document.getElementById('Password').value = "" + xhttp.responseText;
                         document.getElementById('UserName').value = "technician";
+                        // After filling out credentials, verify that our password has succesfully filled and is of proper length
+                        // If yes, click the login button to automatically sign in
                         if (document.getElementById('Password').value != null && String(document.getElementById('Password').value).length == 10) {
                             document.querySelector('.submitBtn').click();                        
                         }
                     }
                 }
+                // Actually send our XHR request
                 xhttp.open("GET", "http://127.0.0.1/potd", true);
                 xhttp.send();
             }                    
--- Chrome/iglass.js
+++ Chrome/iglass.js
@@ -1,8 +1,24 @@
 var contentDiv = document.getElementById("content");
+var outageDiv = document.getElementById("outages");
+
+if (typeof(outageDiv) != undefined && outageDiv != null) {
+    for (i = 0; i < outageDiv.getElementsByTagName("li").length; i++) {
+        var text = outageDiv.getElementsByTagName("li")[i].innerText;
+        console.log(i + " : " + outageDiv.getElementsByTagName("li")[i].innerHTML);
+        var outTime = text.substr((text.length -6));
+        outTime = outTime.split(")");
+        var timeArray = [0, 0, 0];
+        console.log(outTime[0]);
+    }
+}
+else {
+    console.log("No outages reported");
+}
 
 if (document.getElementsByClassName("clickForPopup").length > 0) {
     var signals = document.getElementsByClassName("clickForPopup");
-
+    
+    // Upstream
     var tx = signals[0].getElementsByTagName("span")[0].innerText;
     var usnr = signals[1].getElementsByTagName("span")[0].innerText;
     
@@ -17,9 +33,6 @@ if (document.getElementsByClassName("clickForPopup").length > 0) {
     
     var signalButton = "<button id='signalButton' class='ui-button ui-corner-all ui-widget' onclick='copySignal()'>Copy Signals</button>";
     document.getElementById('expandAll').insertAdjacentHTML('beforebegin', signalButton);
-}
-else {
-    console.log("Not a modem page");
 }
 
 function copySignal() {
