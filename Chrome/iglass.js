61233aff7b44639d27ba8ae48e3802086e7ff0e1
Shea Ramage <shea.ramage@gmail.com>
2019-01-30 16:15:49 -0500

    add to iGlass outage functionality

--- Chrome/iglass.js
+++ Chrome/iglass.js
@@ -3,6 +3,9 @@ var outageDiv = document.getElementById("outages");
 
 if (typeof(outageDiv) != undefined && outageDiv != null) {
     var outageCount = 0;
+    var mTotal = 0;
+    var hTotal = 0;
+    var dTotal = 0;
     for (i = 0; i < outageDiv.getElementsByTagName("li").length; i++) {
         // Get full outage string
         var liStr = outageDiv.getElementsByTagName("li")[i].innerText;
@@ -11,6 +14,26 @@ if (typeof(outageDiv) != undefined && outageDiv != null) {
         var spanStr = outageDiv.getElementsByTagName("span")[i].innerText;
         console.log(i + " : " + liHTML);
         console.log(spanStr);
+        // Split the outage time values by colon to get minute/hour/day values
+        // We can't 
+        var spanArray = spanStr.split(':');
+        if (spanArray.length == 3) {
+            var days = spanArray[0]; 
+            var hours = spanArray[1];
+            var minutes = spanArray[2];
+        }
+        else {
+            var hours = spanArray[0];
+            var minutes = spanArray[1];
+        }
+        mTotal += minutes
+        hTotal += hours
+        if (days != null && days != 0) {
+            dTotal += days;
+        }
+
+        console.log(spanStr.split(':')[0] + ' this should be before colon');
+        console.log(spanStr.split(':')[1] + ' this should be after colon');
         var timeArray = [0, 0, 0];
         outageCount++;
     }
@@ -36,17 +59,18 @@ if (document.getElementsByClassName("clickForPopup").length > 0) {
     var downstr = "Downstream Power: " + rx + ", SNR: " + dsnr;
     var signalstr = upstr + " || " + downstr;
     
-    var signalButton = "<button id='signalButton' class='ui-button ui-corner-all ui-widget' onclick='copySignal()'>Copy Signals</button>";
+    var signalButton = "<button id='signalButton' class='ui-button ui-corner-all ui-widget' onclick='copyInfo(signalstr, \"signals\")'>Copy Signals</button>";
     document.getElementById('expandAll').insertAdjacentHTML('beforebegin', signalButton);
 }
 
-function copySignal() {
-    var signalArea = document.createElement('textarea');
-    signalArea.value = signalstr;
-    contentDiv.appendChild(signalArea);
-    signalArea.style.height = "0px";
-    signalArea.style.width = "0px";
-    signalArea.style.opacity = "0";
-    signalArea.select();
+function copyInfo(values, name) {
+    var infoArea = document.createElement('textarea');
+    infoArea.id = name;
+    infoArea.value = values;
+    contentDiv.appendChild(infoArea);
+    infoArea.style.height = "0px";
+    infoArea.style.width = "0px";
+    infoArea.style.opacity = "0";
+    infoArea.select();
     document.execCommand("copy");
 }
