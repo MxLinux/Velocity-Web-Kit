71e0b39afaab094079981c9d48578e64f06d6d36
Shea Ramage <shea.ramage@gmail.com>
2019-08-30 13:28:23 -0400

    Stuff

--- Chrome_v2/static/js/iglass.js
+++ Chrome_v2/static/js/iglass.js
@@ -1,4 +1,4 @@
 // Get values
 // Let's start with getting the major elements containing important info
-const allDiv = document.getElementsByTagName("div");
+const allDiv = document.getElementsByClassName("ui-widget");
 console.log(allDiv);
