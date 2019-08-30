2ecc3c52ddbd59036259ca44bb1d7e9557b1faec
Shea Ramage <shea.ramage@gmail.com>
2019-08-30 16:33:49 -0400

    Begin refactor of extension because the original blows

--- Chrome_v2/static/js/iglass.js
+++ Chrome_v2/static/js/iglass.js
@@ -1,4 +1,7 @@
 // Get values
 // Let's start with getting the major elements containing important info
 const allDiv = document.getElementsByClassName("ui-widget");
-console.log(allDiv);
+for (i = 0; i < allDiv.length; i++) {
+    console.log(allDiv[i].innerHTML);
+}
+console.log(allDiv.length);
