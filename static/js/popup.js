67169b17b683c28e4505cd55a53505bbbf14d083
Thomas <thomas.m.ramage@gmail.com>
2019-10-15 17:02:25 -0700

    Things

--- static/js/popup.js
+++ static/js/popup.js
@@ -3,4 +3,11 @@ chrome.tabs.getSelected(null, function(tab) {
     document.getElementById("content").textContent = "Current tab: " + tabID;
 })
 
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+      console.log(response.farewell);
+    });
+});
+
 // maybe send a message to tab requesting any script features, display their respective toggles and settings
+// popup possibly tabbed by "this page", "settings", and feature toggle
