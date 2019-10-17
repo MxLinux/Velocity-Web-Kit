39e6f43438ec2c7f6e8e63001cbe57232246512f
Shea Ramage <shea.ramage@gmail.com>
2019-10-17 15:54:49 -0400

    So much nonsense

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,6 +1,35 @@
-chrome.tabs.getSelected(null, function(tab) {
-    tabID = tab.id;
-    document.getElementById("content").textContent = "Current tab: " + tabID;
-})
+const query = { active: true, currentWindow: true };
+
+function callback(tabs) {
+    var currentTab = tabs[0];
+    console.log("tab is " +currentTab.url);
+    return(currentTab);
+}
+
+var tabQuery = chrome.tabs.query(query, callback);
+console.log("tab query: " + JSON.stringify(tabQuery));
+var tabURL = tabQuery.url;
+var tabID = tabQuery.id;
+
+/*function setVTTView() {
+    location.href="asdlfaskldjfa";
+}
+
+function setTicketView() {
+    console.log("Stuff");
+}
+
+const iGlassRegex = /(?<=https\:\/\/)(.*?)(?=\?)/g;
+
+
+if (tabURL === "http://eusvr41/internettechsupport/Overview.aspx") {
+    window.location.reload();
+}
+else if (tabURL.match(iGlassRegex) === "noc.iglass.net/jglass/igo/devInfo.htm") {
+    console.log("Yay!");
+}
+console.log("oof" +tabURL);
+console.log("uuf" +iGlassRegex);
 
 // maybe send a message to tab requesting any script features, display their respective toggles and settings
+*/
