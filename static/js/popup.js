723eba8327ddc8f4e431b03b24729279f5d0f9af
Thomas <thomas.m.ramage@gmail.com>
2019-11-18 22:07:37 -0500

    Work on popup

--- static/js/popup.js
+++ static/js/popup.js
@@ -6,17 +6,26 @@ function setTicketView() {
     console.log("Stuff");
 }
 
-const iGlassRegex = /(?<=https\:\/\/)(.*?)(?=\?)/g;
+// Regex matching for various URLs
+const VTTRegex = /http:\/\/eusvr41\/internettechsupport\/Overview\.aspx*/g;
+const TicketRegex = /http:\/\/eusvr41\/internettechsupport\/SearchTicket\.aspx\?TicketID=*/g;
+const CustSummRegex = /http:\/\/eusvr70\/customersummary\/CustomerPage\.aspx\?/g;
+const iGlassRegex = /https:\/\/noc\.iglass\.net\/jglass\/igo\/devInfo.htm*/g;
+// Web applications matching older ARRIS models 1670/2470/2472/etc.
+const GatewayRegex = /https?:\/\/10.*:8080\/*/g;
+// Web applications matching newer ARRIS models - 3450 is the only we currently use
+const NewGatewayRegex = /https?:\/\/10\.*\/login\.php/g;
 
-document.addEventListener("DOMContentLoaded", function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        // Note: this requires "activeTab" permission to access the URL
-        if (tabs[0].url.match(iGlassRegex) == "noc.iglass.net/jglass/igo/devInfo.htm") {
-            console.log("Things and stuff idk?");
-            document.querySelector("#content").innerHTML("Hay");
-        }
-        else if (tabs[0].url === "Placeholder") {
-            console.log("Placeholder");
-        }
-    });
-});
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    currentTab = tabs[0];
+    var tabURL = currentTab.url;
+    console.log(tabURL);
+    if(tabURL.match(iGlassRegex)) {
+        console.log("YEP!!!");
+        document.querySelector("#title").innerText = "iGlass Settings";
+    }
+    else {
+        console.log("NOPE, BUT KINDA YEP");
+    }
+})
